1ecf7e60ccbec910f4a6bd840c3c29ea70814758
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-20 15:56:40 -0300

    fiz uma alteração no texto do h2 da fase1 do personagem 2

---
+++ verdadesecreta1.js
@@ -0,0 +1,12 @@
+function botaoClicado(){
+    var verdadesecreta = prompt("Escolha uma opcao")
+    if(verdadesecreta == 1){
+        window.location.href = "./verdadesecreta2.html"
+    }else if(verdadesecreta == 2){
+        window.location.href = "./gameover.html"
+    }else{
+    alert("opcao invalida")
+    botaoClicado()
+    }
+    
+}
