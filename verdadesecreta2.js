ec018bd1980ca946d0d68b1a1058a48c4edbf204
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-20 16:07:49 -0300

    fiz uma alteração na var da function botaoClicado

---
+++ verdadesecreta2.js
@@ -0,0 +1,12 @@
+function botaoClicado(){
+    var resposta = prompt("Escolha uma opcao")
+    if(resposta == 1){
+        window.location.href = "./verdadesecreta2.html"
+    }else if(resposta == 2){
+        window.location.href = "./gameover.html"
+    }else{
+    alert("opcao invalida")
+    botaoClicado()
+    }
+    
+}
