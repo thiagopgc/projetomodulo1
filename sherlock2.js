e49bbf37af39ad847f82ca707f558cb3fd60bd93
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-20 18:26:00 -0300

    criado um js para fase 2 do personagem sherlock

--- sherlock2.js
+++ sherlock2.js
@@ -1,7 +1,7 @@
 function botaoClicado(){
     var resposta = prompt("Escolha uma opcao")
     if(resposta == 1){
-        window.location.href = "./sherlock2.html"
+        window.location.href = "./sherlock3.html"
     }else if(resposta == 2){
         window.location.href = "./gameover.html"
     }else{
