12361d578b8d2fe15cd727fd5e66b9f2914d64c2
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-20 17:34:20 -0300

    fiz alteração no if e else if mudando o local que o usuario vai chegar dependendo da resposta.

--- verdadesecreta1.js
+++ verdadesecreta1.js
@@ -1,9 +1,9 @@
 function botaoClicado(){
     var verdadesecreta = prompt("Escolha uma opcao")
     if(verdadesecreta == 1){
-        window.location.href = "./verdadesecreta2.html"
-    }else if(verdadesecreta == 2){
         window.location.href = "./gameover.html"
+    }else if(verdadesecreta == 2){
+        window.location.href = "./verdadesecreta2.html"
     }else{
     alert("opcao invalida")
     botaoClicado()
--- verdadesecreta2.js
+++ verdadesecreta2.js
@@ -1,7 +1,7 @@
 function botaoClicado(){
     var resposta = prompt("Escolha uma opcao")
     if(resposta == 1){
-        window.location.href = "./verdadesecreta2.html"
+        window.location.href = "./verdadesecreta3.html"
     }else if(resposta == 2){
         window.location.href = "./gameover.html"
     }else{
