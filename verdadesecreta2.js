00771ece2d47392f981b88558f35391e8660be33
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-20 17:37:45 -0300

    alterei o window.locaction.href do  if e else if do verdadesecreta2.js

--- verdadesecreta2.js
+++ verdadesecreta2.js
@@ -1,9 +1,9 @@
 function botaoClicado(){
     var resposta = prompt("Escolha uma opcao")
     if(resposta == 1){
-        window.location.href = "./verdadesecreta3.html"
-    }else if(resposta == 2){
         window.location.href = "./gameover.html"
+    }else if(resposta == 2){
+        window.location.href = "./verdadesecreta3.html"
     }else{
     alert("opcao invalida")
     botaoClicado()
