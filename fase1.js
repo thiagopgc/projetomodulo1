2ca57bec2ff1d4644debde4322cee99bb21136a5
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-21 20:59:51 -0300

    foi criado um laço de repatição (while) para fase1.html.

--- fase1.js
+++ fase1.js
@@ -1,14 +1,21 @@
 function botaoClicado(){
-var resposta = prompt("Escolha uma opcao")
-if(resposta == 1){
-    window.location.href = "./fase2.html"
-}else if(resposta == 2){
-    window.location.href = "./gameover.html"
-}else{
-alert("opcao invalida")
-botaoClicado()
-}
+var resposta
+while(resposta != 1 || resposta != 2)
+{
+    
+    resposta = prompt ('digite uma opção')
+    if(resposta == 1){
+        window.location.href = "./fase2.html"
+        break
+    }else if(resposta == 2){
+        window.location.href = "./gameover.html"
+        break
+    }else{
+    alert("Digite 1 ou 2")
+    break
+   }
+
 
 }
-
+}
 
