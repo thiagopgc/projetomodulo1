caac533648c8439c1f2713fe5eb72455bbfdcd34
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-21 18:50:13 -0300

    adicionadio uma function ganhador.html para ligar ao js atraves do onclick.

--- verdadesecreta1.js
+++ verdadesecreta1.js
@@ -1,8 +1,8 @@
 function botaoClicado(){
-    var verdadesecreta = prompt("Escolha uma opcao")
-    if(verdadesecreta == 1){
+    var resposta = prompt("Escolha uma opcao")
+    if(resposta == 1){
         window.location.href = "./gameover.html"
-    }else if(verdadesecreta == 2){
+    }else if(resposta == 2){
         window.location.href = "./verdadesecreta2.html"
     }else{
     alert("opcao invalida")
