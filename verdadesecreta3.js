94d885cb2e8d5b8cd881485c85dbe01d0872b33e
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-21 21:31:48 -0300

    criado um arquivo js com laço de repetição para verdadesecreta3.html

---
+++ verdadesecreta3.js
@@ -0,0 +1,20 @@
+function botaoClicado(){
+    var resposta
+    while(resposta != 1 || resposta != 2)
+    {
+        
+        resposta = prompt ('digite uma opção')
+        if(resposta == 1){
+            window.location.href = "./ganhador2.html"
+            break
+        }else if(resposta == 2){
+            window.location.href = "./gameover.html"
+            break
+        }else{
+        alert("Digite 1 ou 2")
+        break
+       }
+    
+    
+    }
+    }
